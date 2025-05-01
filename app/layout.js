c8e7dce356fc71e2046173ff73a545e9e433e836
napoleontsiouris <napoleontsiouris13@gmail.com>
2025-05-01 23:14:42 +0300

    change fonts

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${robotoMono.variable} bg-gray-100 text-gray-900`}>
+      <body className={`bg-gray-100 text-gray-900`}>
         <Header />
           {children}
         <Footer />
